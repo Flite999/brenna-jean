509b883d0508e335c17a8d2eca728c0598b4eab2
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2021-08-20 19:36:23 -0400

    fix build err with contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+
+const Contact = () => (
+    <Layout>
+
+        <h1>Woah!</h1>
+
+    </Layout>
+)
+
+export default Contact
